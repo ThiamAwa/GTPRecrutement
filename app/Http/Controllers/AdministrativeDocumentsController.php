eb13487064e3f719c21dd18fb052242b342ee88f
ThiamAwa <awa96362@gmail.com>
2024-08-23 12:20:54 +0000

    3eme commit

---
+++ app/Http/Controllers/AdministrativeDocumentsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\administrative_documents;
+use App\Http\Requests\Storeadministrative_documentsRequest;
+use App\Http\Requests\Updateadministrative_documentsRequest;
+
+class AdministrativeDocumentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeadministrative_documentsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(administrative_documents $administrative_documents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(administrative_documents $administrative_documents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateadministrative_documentsRequest $request, administrative_documents $administrative_documents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(administrative_documents $administrative_documents)
+    {
+        //
+    }
+}
